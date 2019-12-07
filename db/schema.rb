5756e9013755c4845dca40c27ca603cc33f359e0
noel99l <noel1991@hotmail.co.jp>
2019-12-07 05:52:02 +0000

    add has_many & belongs_to

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_07_032814) do
+ActiveRecord::Schema.define(version: 2019_12_07_050921) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.string "postal_code", default: "", null: false
+    t.string "address", default: "", null: false
+    t.string "receiver_name", default: "", null: false
+    t.boolean "is_main_active", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +32,14 @@ ActiveRecord::Schema.define(version: 2019_12_07_032814) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.integer "item_id", null: false
+    t.integer "item_count", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "customers", force: :cascade do |t|
@@ -40,6 +58,49 @@ ActiveRecord::Schema.define(version: 2019_12_07_032814) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.boolean "is_active", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "genre_id", null: false
+    t.string "name", default: "", null: false
+    t.text "description", default: "", null: false
+    t.text "image_id", default: "", null: false
+    t.integer "item_status", default: 0, null: false
+    t.integer "price", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "item_id", null: false
+    t.integer "order_id", null: false
+    t.integer "making_status", default: 0, null: false
+    t.integer "item_count", null: false
+    t.integer "ordered_price", null: false
+    t.string "ordered_item_name", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.integer "fee", default: 800, null: false
+    t.integer "payment_method", null: false
+    t.float "tax_rate", default: 0.1, null: false
+    t.integer "order_status", default: 0, null: false
+    t.string "ordered_reveiver_name", default: "", null: false
+    t.integer "ordered_postal_code", null: false
+    t.string "ordered_address", default: "", null: false
+    t.integer "billing_amount", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
