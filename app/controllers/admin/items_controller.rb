ad92c4ca95687540a806d3e6c3ebf04348c40286
yasan <track400m47@gmail.com>
2019-12-09 04:00:40 +0000

    Add admin側　 商品一覧　商品登録画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,11 +1,16 @@
 class Admin::ItemsController < AdminController
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
   end
 
   def show
@@ -15,6 +20,11 @@ class Admin::ItemsController < AdminController
   end
 
   def update
+
   end
-  
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :price, :genre_id)
+  end
 end
