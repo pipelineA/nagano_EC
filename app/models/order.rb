3fd57c069da9e58259312306a58a4aa27f7e3167
K.K <gokennsirou@yahoo.co.jp>
2019-12-10 04:34:29 +0000

    Add admin order show page

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,15 @@
 class Order < ApplicationRecord
 	has_many :order_items
 	belongs_to :user
+  accepts_nested_attributes_for :order_items
+  enum payment_method: { クレジットカード: 0, 銀行振込: 1 }
+  enum order_status: { 入金待ち: 0, 発送待ち: 1, 発送済み: 2 }
+
+  def amount
+    sum = 0
+    order_items.each do |order_item|
+      sum += order_item.item.price * (1 + tax_rate) * order_item.item_count
+    end
+    sum.floor
+  end
 end
