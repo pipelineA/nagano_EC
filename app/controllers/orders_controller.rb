60047129e8cd1c2c76787e435613d449bfdc4a69
hrtm23hrtm4h2 <tmhr24@gmail.com>
2019-12-12 06:43:10 +0000

    top_update

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,16 +2,13 @@ class OrdersController < ApplicationController
   def new
     @order = Order.new
     @user = User.all
-    @address = Address.new
     @main_address = current_user.addresses.find_by(is_main_address: true)
     @addresses = current_user.addresses.where(is_main_address: false)
 
   end
 
   def confirm
-    @cart_items = current_user.cart_items
     @order = current_user.orders.build(order_params)
-    @order.order_items.build
     if params[:order][:address_type] == "address1"
       main_address = current_user.addresses.find_by(is_main_address: true)
       @order.ordered_address = main_address.address
@@ -24,13 +21,14 @@ class OrdersController < ApplicationController
       @order.ordered_receiver_name = selected_address.receiver_name
     elsif params[:order][:address_type] == "address3"
     end
+    @cart_items = current_user.cart_items
+    @main_address = current_user.addresses.find_by(is_main_address: true)
   end
 
   def create
-    order = current_user.orders.build(order_params)
+    order = current_user.order.build(order_params)
+    order.user_id = current_user.id
     order.save
-    cart_items = current_user.cart_items
-    cart_items.destroy_all
     redirect_to orders_thanks_path
   end
 
@@ -49,8 +47,7 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order).permit(:user_id, :fee, :payment_method, :tax_rate,
                                   :order_status, :ordered_receiver_name, :ordered_postal_code,
-                                  :ordered_address, :billing_amount, 
-                                  order_items_attributes: [:id, :item_id, :order_id, :item_count, :ordered_price, :ordered_item_name])
+                                  :ordered_address, :billing_amount, order_items_attributes: [:id, :item_id, :order_id, :item_count, :ordered_price, :ordered_item_name])
 
   end
 end
