62e277523d42563b961deedd39b5ec103525b585
K.K <gokennsirou@yahoo.co.jp>
2019-12-12 10:25:40 +0000

    Add admin_login_required

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,8 @@
 class AdminController < ApplicationController
+  before_action :login_required
+
+  private
+  def login_required
+    redirect_to new_admin_session_path unless current_admin
+  end
 end
