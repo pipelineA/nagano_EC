9d0ecd7640344a31836c76a424010f086645f55f
hrtm23hrtm4h2 <tmhr24@gmail.com>
2019-12-15 08:22:32 +0000

    user_edit_validates

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,9 @@
 class Address < ApplicationRecord
 	belongs_to :user
+
+	validates :postal_code, presence: true, format: { with: /\A[0-9]+\z/ }, length: { is: 7 }
+    validates :address, presence: true
+
 	def address_select
     "〒""#{self.postal_code.insert(3, "-")} ""#{self.address} " " #{self.receiver_name}"
     end
