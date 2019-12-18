fc1521c43a0da92629042959c3b6d29a909ff610
K.K <gokennsirou@yahoo.co.jp>
2019-12-18 12:06:23 +0000

    注文履歴、マイページ

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -59,7 +59,7 @@ class OrdersController < ApplicationController
   end
 
   def index
-    @orders = current_user.orders
+    @orders = current_user.orders.page(params[:page]).per(10).order(created_at: :desc)
   end
 
   def show
