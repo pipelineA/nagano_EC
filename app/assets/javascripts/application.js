e5457516c3ab9a4883c54dd30d0a28c8d94a7a0a
K.K <gokennsirou@yahoo.co.jp>
2019-12-19 08:14:20 +0000

    parent

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -51,17 +51,17 @@ $(document).on('turbolinks:load', function(){
     // $(this).parent()で、クリックされた".edit-button"の親要素の<td>が取得できる。
     // $(this).parent().parent()で、クリックされた".edit-button"の親要素の<td>のさらに親要素の<tr>が取得できる。
     // 取得した<tr>を$parentという変数に代入する。
-    	let $parent = $(this).parent().parent();
+    	let parent = $(this).parent().parent();
 
     // $parent.find('クラス名')で、$parentで指定した<tr>の中にある'クラス名'だけを取得することができる。
     // （これを指定しない場合は、クリックした<tr>以外の列の'クラス名'にまで、影響が及んでしまう）
 
     // それぞれの'クラス名'のボタンなどに、'none-active'クラスを追加したり削除したりすることで、表示・非表示を切り替えることができる。
     // （'none-active'クラスには、cssで{display: none;}を設定している）
-    	$parent.find('.edit-button').addClass('none-active');
-    	$parent.find('.submit-button').removeClass('none-active');
-    	$parent.find('.genre-text').addClass('none-active');
-    	$parent.find('.genre-form').removeClass('none-active').focus();
+    	parent.find('.edit-button').addClass('none-active');
+    	parent.find('.submit-button').removeClass('none-active');
+    	parent.find('.genre-text').addClass('none-active');
+    	parent.find('.genre-form').removeClass('none-active').focus();
     	return false;
     });
 
