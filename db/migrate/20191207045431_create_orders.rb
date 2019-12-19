a491679ed0b9186fcbdc16e9d28e914399f285ca
K.K <gokennsirou@yahoo.co.jp>
2019-12-19 08:34:37 +0000

    デフォルト値を変える

--- db/migrate/20191207045431_create_orders.rb
+++ db/migrate/20191207045431_create_orders.rb
@@ -1,15 +1,15 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.integer :customer_id, null: false, default: ""
+      t.integer :customer_id, null: false
       t.integer :fee, null: false, default: 800
       t.integer :payment_method, null: false, default: ""
       t.float :tax_rate, null: false, default: 0.1
       t.integer :order_status, null: false, default: 0
       t.string :ordered_reveiver_name, null: false, default: ""
-      t.integer :ordered_postal_code, null: false, default: ""
+      t.integer :ordered_postal_code, null: false
       t.string :ordered_address, null: false, default: ""
-      t.integer :billing_amount, null: false, default: ""
+      t.integer :billing_amount, null: false
       t.timestamps
     end
   end
